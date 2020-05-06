97f55d7fd67fd046fd309e274eca60d1d8cb90f1
Pawel <ssopel10@gmail.com>
2020-05-07 01:01:35 +0200

    add function to show pop-up

---
+++ scripts/showPopUp.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.header__button');
+const popUp = document.querySelector('.popUp')
+
+const showPopUp = () => {
+    popUp.classList.toggle('popUp--active');
+}
+
+button.addEventListener('click', showPopUp)
