0d70cc79f481131ddfc28575ebb4adb1dc649b01
Pawel <ssopel10@gmail.com>
2020-05-07 16:09:46 +0200

    fix errors

--- scripts/changeTabs.js
+++ scripts/changeTabs.js
@@ -1,16 +1,26 @@
 const nav = document.querySelector('.nav__list');
 const container = document.querySelector('.main__container');
+const filter = document.querySelector('.main__filterBar')
+let activeTab = 'recommended';
 
 const changeTab = e => {
-    const menu = [...nav.children];
-    menu.map(el => el.classList.remove('nav__list__element--active'))
-    e.target.classList.toggle('nav__list__element--active')
-    if(e.target.dataset.tabName==='recommended'){
-        container.classList.add('main__container--recommended');
+    if(e.target.classList.contains('nav__list__element')){
+        const menu = [...nav.children];
+        menu.map(el => el.classList.remove('nav__list__element--active'))
+        e.target.classList.add('nav__list__element--active');
+        activeTab = e.target.dataset.tabName;
+        console.log(activeTab);
+        if(activeTab === 'all'){
+            container.classList.add('main__container--all');
+            filter.classList.add('main__filterBar--active');
+        }
+        else{
+            container.classList.remove('main__container--all');
+            filter.classList.remove('main__filterBar--active');
+        }
+
     }
-    else{
-        container.classList.remove('main__container--recommended');
-    }
+    
 }
 
 nav.addEventListener('click', changeTab)
